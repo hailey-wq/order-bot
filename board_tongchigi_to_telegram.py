c803a78051a613ab122aa70ffa17f70876173463
hailey-wq <hailey@route185.com>
2026-09-18 11:18:58 +0900

    Add files via upload

--- board_tongchigi_to_telegram.py
+++ board_tongchigi_to_telegram.py
@@ -54,7 +54,7 @@ SOURCES = [
             "sell": None,
             "moc_buy": None,
             "moc_sell": None,
-            "sniper": "sniper!L5:O1000",
+            "sniper": "sniper!L4:O1000",
         },
     },
 ]
